4baf354a1805ad9b9b2a4ec0f4d2f161bd23cfa0
Marwane07 <marwanelahfidi@gmail.com>
2022-10-18 12:30:10 +0100

    second commit

--- src/coffee/coffee.controller.ts
+++ src/coffee/coffee.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { Coffee } from 'entities/coffee.entity';
 import { get } from 'http';
 import { CoffeeService } from './coffee.service';
 
@@ -7,28 +8,32 @@ export class CoffeeController {
   constructor(private readonly coffeeService: CoffeeService) {}
 
   @Get()
-  sayHello() {
-    return "Hello from Coffee!";
+  sayHello(@Query('id') query : any) {
+    return query;
   }
-  @Get(':id/:key')
+  @Get('key/:key')
   getById(@Param() param : any) {
     //return param;
     const {id, key} = param;
     return id + key;
   }
   @Post()
-  savePost(@Body() body : any) : any { 
-    const {username, password} = body ; 
-    return username + password;
+  saveCoffee(@Body() body : Coffee) : any { 
+      return this.coffeeService.saveCoffee(body);
   }
-  @Delete(':id')
-  deleteCoffee(@Param('id') id : any)  { 
-    let listCoffee = this.coffeeService.data;
-    //console.log(id)
-    let list = listCoffee.filter(obj => obj.id != id)
-    //console.log(index)
-    //listCoffee.splice(index, 1)
-    //console.log(listCoffee)
-    return list
+
+  @Delete(':id')//method delete = localhost:3000/coffee/1
+  deleteCoffee(@Param('id') id : number)  { 
+    return this.coffeeService.deleteCoffee(id);
+  }
+
+  @Put(':id')
+  updateCoffee(@Param('id') id : any, @Body() body : Coffee)  { 
+    return this.coffeeService.updateCoffee(id, body)
+  }
+  @Get('getCoffee/:id')
+  getCoffeeById(@Param('id') id : number) {
+    
+    return this.coffeeService.getById(id);
   }
 }
--- src/coffee/coffee.service.ts
+++ src/coffee/coffee.service.ts
@@ -13,6 +13,29 @@ export class CoffeeService {
         {
             id : 3, flavors : 'vanilla', brand : 'coffec', name : '1MAR Normal'
         }
-    ]
+    ];
+    deleteCoffee(id : number)  { 
+        let list = this.data.filter(obj => obj.id != id);
+        return list;
+    }
+    updateCoffee (id : number, coffee : Coffee){
+        const indexOfObject = this.data.findIndex(object => {
+            return object.id == id;
+          });
+          //console.log(indexOfObject);
+        this.data[indexOfObject].flavors = (coffee.flavors ?? this.data[indexOfObject].flavors);
+        this.data[indexOfObject].brand = coffee.brand;
+        this.data[indexOfObject].name = coffee.name;
+        return this.data;
+    }
+
+    getById(id : number)  { 
+        let coffee = this.data.find(obj => obj.id == id);
+        return coffee;
+    }
+    saveCoffee(coffee : Coffee)  { 
+        this.data.push(coffee)
+        return this.data;
+    }
 
 }
